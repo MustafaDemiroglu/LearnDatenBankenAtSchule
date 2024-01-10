1f66a7b8f8142b100e51f0951714d90986de7279
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-01-10 13:53:36 +0100

    update SELECT

--- SQL/1_SELECT.sql
+++ SQL/1_SELECT.sql
@@ -1,3 +1,27 @@
 SELECT * FROM Customers;
 
 SELECT CustomerName, City FROM Customers;
+
+SELECT TOP 3 * FROM Customers;
+
+SELECT TOP number|percent column_name(s)
+FROM table_name
+WHERE condition;
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+LIMIT number;
+
+SELECT column_name(s)
+FROM table_name
+ORDER BY column_name(s)
+FETCH FIRST number ROWS ONLY;
+
+SELECT column_name(s)
+FROM table_name
+WHERE ROWNUM <= number;
+
+SELECT *
+FROM (SELECT column_name(s) FROM table_name ORDER BY column_name(s))
+WHERE ROWNUM <= number;

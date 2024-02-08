dfc44afdb3737252f3690dd6d57ef9850ee35eb2
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-08 14:54:47 +0100

    join sql

--- SQL/15_JOIN.sql
+++ SQL/15_JOIN.sql
@@ -6,3 +6,26 @@ INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 SELECT ProductID, ProductName, CategoryName
 FROM Products
 INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID;
+
+-- The following SQL statement selects all orders with customer and shipper information:
+-- Example
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+
+-- The following SQL statement will return all employees, and any orders they might have placed:
+-- ExampleGet your own SQL Server
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+-- SQL Self Join Example
+-- The following SQL statement matches customers that are from the same city:
+-- ExampleGet your own SQL Server
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
